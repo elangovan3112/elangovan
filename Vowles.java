e381af55c2040d9b2dc5b44f8cf3334eb3c6a63b
elangovan3112 <139547914+elangovan3112@users.noreply.github.com>
2023-07-24 16:12:58 +0530

    Update Vowles.java

--- Vowles.java
+++ Vowles.java
@@ -1,11 +1,27 @@
-package Inheritance_day3;
-
-public class Vowles {
-	
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+public class CountVowelConsonant {    
+    public static void main(String[] args) {    
+            
+           
+        int vCount = 0, cCount = 0;    
+            
+            
+        String str = "This is a really simple sentence";    
+            
+           
+        str = str.toLowerCase();    
+            
+        for(int i = 0; i < str.length(); i++) {    
+               
+            if(str.charAt(i) == 'a' || str.char //Increments the vowel counter    
+                vCount++;    
+            }    
+             
+            else if(str.charAt(i) >= 'a' && str.charAt(i)<='z') {      
+                    
+                cCount++;    
+            }    
+        }    
+        System.out.println("Number of vowels: " + vCount);    
+        System.out.println("Number of consonants: " + cCount);    
+    }    
+}   
